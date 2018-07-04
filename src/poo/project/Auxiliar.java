ae4b6fc01dbb1222e1c3ae5b3850028e14ac170b
MacroLions <37413420+MacroLions@users.noreply.github.com>
2018-07-04 11:20:10 -0600

    Auxiliar con todos los botones necesarios de Seleccion

--- src/poo/project/Auxiliar.java
+++ src/poo/project/Auxiliar.java
@@ -5,7 +5,9 @@
  */
 package poo.project;
 
+import Imagenes.ImagenFactory;
 import Ventanas.Inicio;
+import Ventanas.Juego;
 import Ventanas.Seleccion;
 import Ventanas.VentanaFactory;
 import java.awt.event.ActionEvent;
@@ -30,9 +32,33 @@ public class Auxiliar {
         Seleccion.getButtonPapa().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                Juego.setSprite(ImagenFactory.getImagen(1));
                 VentanaAModificar.setContentPane(VentanaFactory.getVentana(3));
                 VentanaAModificar.validate();
-                
+            }
+        });
+        Seleccion.getButtonMama().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Juego.setSprite(ImagenFactory.getImagen(2));
+                VentanaAModificar.setContentPane(VentanaFactory.getVentana(3));
+                VentanaAModificar.validate();
+            }
+        });
+        Seleccion.getButtonHermana().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Juego.setSprite(ImagenFactory.getImagen(3));
+                VentanaAModificar.setContentPane(VentanaFactory.getVentana(3));
+                VentanaAModificar.validate();
+            }
+        });
+        Seleccion.getButtonHermano().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Juego.setSprite(ImagenFactory.getImagen(4));
+                VentanaAModificar.setContentPane(VentanaFactory.getVentana(3));
+                VentanaAModificar.validate();
             }
         });
     }
