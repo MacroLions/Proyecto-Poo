2729e659e9c46d1afc54cb26ba4939255bca36f6
MacroLions <37413420+MacroLions@users.noreply.github.com>
2018-07-04 10:10:11 -0600

    Image monstruo

---
+++ src/Personajes/Monstruo.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Personajes;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+
+/**
+ *
+ * @author Maishi
+ */
+public class Monstruo{
+    JLabel ImagenBase = new JLabel(new ImageIcon("path_to_image.png"));
+}
