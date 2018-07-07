f556a6ce7337f18b67d9c55e27e6081314e9e8e9
Nuria Rivas <00041517@uca.edu.sv>
2018-07-06 21:53:52 -0300

    Se modifico el diseño de la ventana consumibles

--- src/Ventanas/Consumibles.java
+++ src/Ventanas/Consumibles.java
@@ -25,20 +25,14 @@ public class Consumibles extends JPanel {
     ImageIcon ObjetoFinalIMG = new ImageIcon("src\\Imagenes\\objetofinal.jpg");
     static JButton ButtonConfirmacion = new JButton("CONFIRMAR");
     static JButton ButtonCancelar = new JButton("CANCELAR");
-    static JButton ButtonVolver = new JButton("VOLVER");
-    static JButton ButtonGalleta = new JButton("10 pts");
-    static JButton ButtonJugo = new JButton("20 pts");
-    static JButton ButtonPupusa = new JButton("30 pts");
-    static JButton ButtonEmpanada = new JButton("25 pts");
-    static JButton ButtonChocolate = new JButton("15 pts");
-    static JButton ButtonSoda = new JButton("40 pts");
-    static JButton ButtonLeche = new JButton("40 pts");
-    static JButton ButtonChurro = new JButton("10 pts");
+    static JButton ButtonMas = new JButton("+");
+    static JButton ButtonMenos = new JButton("-");
+    ;
     //No es necesario que los objetos en ventan sean estaticos, solamente confirmacion y cancelar para regresara a la pantalla anterior
     //Los otros no son necesarios porque la lista de consumbiles de ljugador ES LA QUE DEBE SER STATIC para ser modificada.
     //ALGUNOS BOTONES NO HAN SIDO AGREGADOS, POR FAVOR GUARDE LA PACIENCIA PARA HACER ADD LUEGO NOS VA A PASAR LO MISMO QUE EL MODPACKS();
     //Fakiu
-    JLabel puntosJugador;
+    JLabel puntosJugador, total;
     JLabel lblTitulo, lblGalleta, lblJugo, lblPupusa, lblEmpanada, lblChocolate, lblLista_objetos, lblPuntos_acumulados;
     JLabel lblSoda, lblLeche, lblChurro;
 
@@ -49,71 +43,64 @@ public class Consumibles extends JPanel {
         lblTitulo.setBounds(275, 10, 200, 20);
 
         //EMPIEZAN LOS BOTONES PARA OBTENER CONSUMIBLES
-        lblGalleta = new JLabel("Galleta");
-        lblGalleta.setFont(new Font("Arial", Font.BOLD, 25));
-        lblGalleta.setBounds(50, 98, 400, 30);
+        lblGalleta = new JLabel("Galleta 10 pts");
+        lblGalleta.setFont(new Font("Arial", Font.BOLD, 16));
+        lblGalleta.setBounds(50, 100, 400, 20);
 
-        ButtonGalleta.setBounds(180, 100, 75, 30);
+        ButtonMas.setBounds(240, 100, 50, 20);
 
-        lblJugo = new JLabel("Jugo");
-        lblJugo.setFont(new Font("Arial", Font.BOLD, 25));
-        lblJugo.setBounds(50, 150, 400, 30);
+        lblJugo = new JLabel("Jugo 20 pts");
+        lblJugo.setFont(new Font("Arial", Font.BOLD, 16));
+        lblJugo.setBounds(50, 120, 400, 20);
 
-        ButtonJugo.setBounds(180, 148, 75, 30);
+        ButtonMenos.setBounds(440, 100, 50, 20);
 
-        lblPupusa = new JLabel("Pupusa");
-        lblPupusa.setFont(new Font("Arial", Font.BOLD, 25));
-        lblPupusa.setBounds(50, 200, 400, 30);
+        lblPupusa = new JLabel("Pupusa 30 pts");
+        lblPupusa.setFont(new Font("Arial", Font.BOLD, 16));
+        lblPupusa.setBounds(50, 140, 400, 20);
 
-        ButtonPupusa.setBounds(180, 198, 75, 30);
-
-        lblEmpanada = new JLabel("Empanada");
-        lblEmpanada.setFont(new Font("Arial", Font.BOLD, 25));
-        lblEmpanada.setBounds(50, 250, 400, 30);
-
-        ButtonEmpanada.setBounds(180, 248, 75, 30);
+        lblEmpanada = new JLabel("Empanada 25");
+        lblEmpanada.setFont(new Font("Arial", Font.BOLD, 16));
+        lblEmpanada.setBounds(50, 160, 400, 20);
 
         //consumibles ubicados a la derecha
-        lblSoda = new JLabel("Soda");
-        lblSoda.setFont(new Font("Arial", Font.BOLD, 25));
-        lblSoda.setBounds(375, 98, 400, 30);
+        lblSoda = new JLabel("Soda 40 pts");
+        lblSoda.setFont(new Font("Arial", Font.BOLD, 16));
+        lblSoda.setBounds(50, 180, 400, 20);
 
-        ButtonSoda.setBounds(500, 100, 75, 30);
+        lblLeche = new JLabel("Leche 40 pts");
+        lblLeche.setFont(new Font("Arial", Font.BOLD, 16));
+        lblLeche.setBounds(50, 200, 400, 20);
 
-        lblLeche = new JLabel("Leche");
-        lblLeche.setFont(new Font("Arial", Font.BOLD, 25));
-        lblLeche.setBounds(375, 150, 400, 30);
+        lblChurro = new JLabel("Churro 10 pts");
+        lblChurro.setFont(new Font("Arial", Font.BOLD, 16));
+        lblChurro.setBounds(50, 220, 400, 20);
 
-        ButtonLeche.setBounds(500, 148, 75, 30);
-        
-        lblChurro = new JLabel("Churro");
-        lblChurro.setFont(new Font("Arial", Font.BOLD, 25));
-        lblChurro.setBounds(375, 200, 400, 30);
+        lblChocolate = new JLabel("Chocolate 15 pts");
+        lblChocolate.setFont(new Font("Arial", Font.BOLD, 16));
+        lblChocolate.setBounds(50, 240, 400, 20);
 
-        ButtonChurro.setBounds(500, 198, 75, 30);
-        
-        lblChocolate = new JLabel("Chocolate");
-        lblChocolate.setFont(new Font("Arial", Font.BOLD, 25));
-        lblChocolate.setBounds(375, 250, 400, 30);
-
-        ButtonChocolate.setBounds(500, 248, 75, 30);
+        total= new JLabel("Total de compra ");
+        total.setFont(new Font("Arial", Font.BOLD, 16));
+        total.setBounds(350,300, 200, 20);
         
         puntosJugador = new JLabel("Puntos: "+Jugador.getPuntos());
-        puntosJugador.setBounds(200, 375, 125, 75);
+        puntosJugador.setFont(new Font("Arial", Font.BOLD,16));
+        puntosJugador.setBounds(100, 375, 125, 75);
 
         //TERMINAN LOS BOTONES DE CONSUMIBLES
         //BOTONES DE FUNCION
         ButtonConfirmacion.setBounds(400, 375, 125, 75);
         ButtonCancelar.setBounds(540, 375, 125, 75);
-        ButtonVolver.setBounds(150, 250, 100, 70);
+        
 
         setLayout(null);
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
         add(lblTitulo);
         add(lblGalleta);
-        add(ButtonGalleta);
+        add(ButtonMas);
         add(lblJugo);
-        add(ButtonJugo);
+        add(total);
         add(lblPupusa);
         add(lblEmpanada);
         add(lblChocolate);
@@ -122,14 +109,10 @@ public class Consumibles extends JPanel {
         add(lblChurro);
 
         //agregando botones 
-        add(ButtonPupusa);
-        add(ButtonChocolate);
-        add(ButtonEmpanada);
+        
         add(ButtonConfirmacion);
         add(ButtonCancelar);
-        add(ButtonSoda);
-        add(ButtonLeche);
-        add(ButtonChurro);
+        add(ButtonMenos);
         add(puntosJugador);
 
         validate();
