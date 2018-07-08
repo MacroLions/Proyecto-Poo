993cc9c2f7177d8ec1c0ca0e4d889c7986416f6d
RichardF12 <00168117@uca.edu.sv>
2018-07-08 00:25:31 -0600

    Llamada a musica agregada.

--- src/poo/project/PooProject.java
+++ src/poo/project/PooProject.java
@@ -25,11 +25,12 @@ public class PooProject {
     public static void main(String[] args) {
         JFrame ventana = new JFrame("CREEPY FAMILY RUMBLE");
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        ventana.setContentPane(VentanaFactory.getVentana(4));
+        ventana.setContentPane(VentanaFactory.getVentana(1));
         Auxiliar.configBotones(ventana);
         ventana.setResizable(false);
         ventana.pack();
         ventana.setVisible(true);
+        Music.MainTheme();
     }
     
 }
