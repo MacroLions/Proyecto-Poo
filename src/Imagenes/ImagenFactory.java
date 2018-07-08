d57f05282217c667aac70cc249efbc1c25b1eb8e
MacroLions <37413420+MacroLions@users.noreply.github.com>
2018-07-08 04:00:29 -0600

    Imagenes agregadas y editadas. Imagen Factory ahora cuenta con funcion para imagenes de Backgrounds.

--- src/Imagenes/ImagenFactory.java
+++ src/Imagenes/ImagenFactory.java
@@ -15,9 +15,9 @@ public class ImagenFactory {
      public static ImageIcon getImagen(int opcion){
         switch(opcion){
             case 1:
-                return new ImageIcon("src\\Imagenes\\papacreepy.jpg");             
+                return new ImageIcon("src\\Imagenes\\papacreepy.png");             
             case 2:
-                return new ImageIcon("src\\Imagenes\\mamacreepy.jpg");
+                return new ImageIcon("src\\Imagenes\\mamacreepy.png");
             case 3:
                 return new ImageIcon("src\\Imagenes\\hijacreepy.png");
             case 4:
@@ -25,9 +25,30 @@ public class ImagenFactory {
             case 5:
                 return new ImageIcon("src\\Imagenes\\fin.png");
             case 6:
-                return new ImageIcon("src\\Imagenes\\Lobo.jpg");
+                return new ImageIcon("src\\Imagenes\\Lobo.png");
             case 7:
                 return new ImageIcon("src\\Imagenes\\TextBox.png");
+            case 8:
+                return new ImageIcon("src\\Imagenes\\BatallaBG.png");
+            case 9:
+                return new ImageIcon("src\\Imagenes\\SelPapa.png");
+            case 10:
+                return new ImageIcon("src\\Imagenes\\SelMama.png");
+            case 11:
+                return new ImageIcon("src\\Imagenes\\SelHija.png");
+            case 12:
+                return new ImageIcon("src\\Imagenes\\SelHijo.png");
+        }
+        return null;
+    }
+     
+     public static ImageIcon getBackground(int opcion){
+        switch(opcion){
+            case 1:
+                return new ImageIcon("src\\Imagenes\\BatallaBG.png");             
+            case 2:
+                return new ImageIcon("src\\Imagenes\\SelBG.jpg");
+            
         }
         return null;
     }
