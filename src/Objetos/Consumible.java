7df6bd451cf228f55692698b0f6747e90144cedb
Nuria Rivas <00041517@uca.edu.sv>
2018-07-09 00:19:33 -0300

    Se agregaron los atributos, setter y getter a la clase consumible

--- src/Objetos/Consumible.java
+++ src/Objetos/Consumible.java
@@ -10,5 +10,28 @@ package Objetos;
  * @author Mai Perez
  */
 public class Consumible {
+    private String nombre;
+    private int vidaRecuperada;
+
+    public Consumible(String nombre, int vidaRecuperada) {
+        this.nombre = nombre;
+        this.vidaRecuperada = vidaRecuperada;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getVidaRecuperada() {
+        return vidaRecuperada;
+    }
+
+    public void setVidaRecuperada(int vidaRecuperada) {
+        this.vidaRecuperada = vidaRecuperada;
+    }
     
 }
