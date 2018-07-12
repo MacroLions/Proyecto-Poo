bf161ed80a90474bfa269ad47f69805070719e2a
MacroLions <00175617@uca.edu.sv>
2018-07-12 15:31:19 -0600

    Librerias extras borradas.

--- src/poo/project/PooProject.java
+++ src/poo/project/PooProject.java
@@ -4,15 +4,7 @@
  * and open the template in the editor.
  */
 package poo.project;
-
-import SFX.Music;
-import Ventanas.Inicio;
-import Ventanas.Seleccion;
 import Ventanas.VentanaFactory;
-import java.awt.ComponentOrientation;
-import java.awt.Container;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 /**
  *
